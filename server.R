638af06be005f3bf2a1b932fd4dd06710d1ac830
Gary Simmons <gary.simmons@colorado.edu>
2021-10-28 17:51:56 -0500

    start adding conditional panels

--- server.R
+++ server.R
@@ -1,7 +1,17 @@
 library(shiny)
 library(dplyr)
 library(ggplot2)
+library(leaflet)
 
 function(input, output){
+  powerplantCountry = reactive({
+    powerplantbycountry = powerplants %>% filter(country_long == input$country_long)
+  })
+  
+  powerplantCompany = reactive({
+    powerplantbycompany = powerplants %>% filter(owner == input$owner)
+  })
+  
+  output$tabName = input$tabselected
   
 }
--- ui.R
+++ ui.R
@@ -3,9 +3,29 @@ library(shinydashboard)
 dashboardPage(
   dashboardHeader(title="Global Power Plant Shiny App"),
   dashboardSidebar(
-    sidebarUserPanel("Gary Garth Simmons",image="GGS_GK.jpeg")
+    sidebarUserPanel("GGSimmons1992",image="GGS_GK.jpeg"),
+    sidebarMenu(
+      radioButtons(
+        inputId="tabselected",label="View by",
+        choices=c("Country","Company")
+      )
+    ),
+    conditionalPanel(condition = output$tabName == "Country",
+                     selectizeInput(inputId="country_long",
+                                    label='country',
+                                    choice= unique(powerplants$country_long))
+                     ),
+    conditionalPanel(condition = output$tabName == "Company",
+                     selectizeInput(inputId="owner",
+                                    label='company',
+                                    choice= unique(powerplants$owner))
+    )
+    
   ),
   dashboardBody(
-    
+    tabItems(
+      tabItem(tabName='Country'),
+      tabItem(tabName = "Company")
+    )
   )
 )
